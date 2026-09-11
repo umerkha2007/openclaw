fbf4d9ec87680ba9584bb01b53b952469516addd
umer.khalid <umer.khalid@colliers.com>
2026-09-10 20:01:43 -0700

    removed workflows

--- .github/actions/ios-signing-keychain/post.mjs
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-
-import { cleanupOwnedKeychain } from "./keychain.mjs";
-
-cleanupOwnedKeychain().catch((error) => {
-  console.error(error instanceof Error ? error.message : String(error));
-  process.exitCode = 1;
-});
